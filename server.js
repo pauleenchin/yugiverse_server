b1aa3002cbde66d8f90a70743d96a6b83b038395
Pauleen Chin <143438451+pauleenchin@users.noreply.github.com>
2025-04-21 09:03:17 -0400

    jdnn

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
-// Setup multer for image uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./public/images/");
@@ -29,7 +28,6 @@ const upload = multer({
   }
 });
 
-// Monster data
 let monsters = [
   {
     id: "blue-eyes-white-dragon",
@@ -52,7 +50,7 @@ let monsters = [
     name: "Elzette of the White Forest",
     image: "images/elzette-of-the-white-forest.jpg",
     level: 2,
-    biotext: "You can send 1 Spell/Trap from your hand or field to the GY; Special Summon this card from your hand, then add 1 'White Forest' monster from your Deck to your hand, except 'Elzette of the White Forest'...",
+    biotext: "You can send 1 Spell/Trap from your hand or field to the GY; Special Summon this card from your hand, then add 1 \"White Forest\" monster from your Deck to your hand, except \"Elzette of the White Forest\"...",
     type: "Spellcaster / Effect",
     attribute: "Light",
     attack: "0",
@@ -76,16 +74,63 @@ let monsters = [
     name: "Toadally Awesome",
     image: "images/toadally-awesome.jpg",
     level: 2,
-    biotext: "Once per turn, during the Standby Phase: You can detach 1 material from this card; Special Summon 1 'Frog' monster from your Deck...",
+    biotext: "Once per turn, during the Standby Phase: You can detach 1 material from this card; Special Summon 1 \"Frog\" monster from your Deck...",
     type: "Aqua / Xyz / Effect",
     attribute: "Water",
     attack: "2200",
     defense: "0",
     cardart: []
+  },
+  {
+    id: "mulcharmy-purulia",
+    name: "Mulcharmy Purulia",
+    image: "images/mulcharmy-purulia.jpg",
+    level: 4,
+    biotext: "If you control no cards (Quick Effect): You can discard this card; apply these effects this turn.\nEach time your opponent Normal or Special Summons a monster(s) from the hand, immediately draw 1 card.\nOnce, during this End Phase, if the number of cards in your hand is more than the number of cards your opponent controls +6, you must randomly shuffle cards from your hand into the Deck so the number in your hand equals the number your opponent controls +6.\nYou can only activate 1 other \"Mulcharmy\" monster effect, the turn you activate this effect.",
+    type: "Aqua / Xyz / Effect",
+    attribute: "Water",
+    attack: "2200",
+    defense: "0",
+    cardart: []
+  },
+  {
+    id: "naturia-exterio",
+    name: "Naturia Exterio",
+    image: "images/naturia-exterio.jpg",
+    level: 10,
+    biotext: "\"Naturia Beast\" + \"Naturia Barkion\"\nA Fusion Summon of this card can only be done with the above Fusion Materials. When a Spell/Trap Card is activated (Quick Effect): You can banish 1 card from your GY, then send the top card of your Deck to the GY; negate the activation, and if you do, destroy that card. This card must be face-up on the field to activate and to resolve this effect.",
+    type: "Beast / Fusion ／ Effect",
+    attribute: "Earth",
+    attack: "2800",
+    defense: "2400",
+    cardart: []
+  },
+  {
+    id: "tornado-dragon",
+    name: "Tornado Dragon",
+    image: "images/tornado-dragon.jpg",
+    level: 4,
+    biotext: "Once per turn (Quick Effect): You can detach 1 material from this card, then target 1 Spell/Trap on the field; destroy it.",
+    type: "Wyrm / Xyz ／ Effect",
+    attribute: "Wind",
+    attack: "2100",
+    defense: "2000",
+    cardart: []
+  },
+  {
+    id: "mystic-tomato",
+    name: "Mystic Tomato",
+    image: "images/mystic-tomato.jpg",
+    level: 4,
+    biotext: "When this card is destroyed by battle and sent to the GY: You can Special Summon 1 DARK monster with 1500 or less ATK from your Deck in Attack Position.",
+    type: "Plant / Effect",
+    attribute: "Dark",
+    attack: "1400",
+    defense: "1100",
+    cardart: ["images/mystic-tomato2.webp"]
   }
 ];
 
-// Forum posts
 let forumPosts = [
   {
     id: 1,
@@ -109,7 +154,6 @@ let forumPosts = [
 
 let nextId = 4;
 
-// Routes
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
